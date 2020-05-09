8f083e71154c9c6f186618fbe9c0b509d2220d6d
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-05-09 15:40:33 -0400

    Binary Search Template I leads to a problem with very large numbers, still work to do.

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,28 @@
 public class Solution {
+
+    // Binary Search Template I (sqrt)
+    public static int mySqrt(int x) {
+
+        int left = 1;
+        int right = x-1;
+        int middle = -1;
+
+        if(x == 1 || x == 0) return x;
+        while(left<= right) {
+            // int mid = left + (right - left) / 2;
+            //middle = (left + right) / 2;
+            middle = left + (right - left) / 2;
+            if(middle*middle < x) {
+                left = middle + 1;
+            } else if(middle*middle > x) {
+                right = middle - 1;
+            } else return middle;
+        }
+        if(middle*middle>x) return middle-1;
+        return middle;
+    }
+
+    // Introduction
     public static int search(int[] nums, int target) {
 
         int left = 0;
@@ -16,9 +40,14 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {-1,0,3,5,9,12};
 
+        // Introduction
+        /*int[] nums1 = {-1,0,3,5,9,12};
         System.out.println(search(nums1, 9));
-        System.out.println(search(nums1, 2));
+        System.out.println(search(nums1, 2));*/
+
+        // Binary Search Template I (sqrt)
+        System.out.println(mySqrt(16));
+        System.out.println(mySqrt(2147395599));
     }
 }
